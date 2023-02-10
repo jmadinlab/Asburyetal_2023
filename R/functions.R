0a607ad5efee3a35f0b925f61ebf282c00c07ec5
Mollie Asbury <masbury@hawaii.edu>
2023-02-10 10:28:15 -1000

    Initial commit

---
+++ R/functions.R
@@ -0,0 +1,93 @@
+
+##### CODE ADAPTED FROM TORRES-PULLIZA ET AL. 2020 #####
+
+# Functions and packages
+library(sp)
+library(raster)
+library(parallel) # For faster processing
+library(rgdal)
+library(plot3D)
+library(mgcv)
+# library(lme4)
+# library(MuMIn)
+library(sgeostat)
+library(jpeg)
+# library(rgeos)
+library(fields)
+
+# Function for calculating variation in windows
+fd_func <- function(x, y, s) {
+  bx <- extent(cbind(c(x0 + x, y0 + y), c(x0 + x + s, y0 + y + s)))
+  return(diff(range(getValues(crop(data, bx)), na.rm=TRUE)))
+}
+
+R_func <- function(H0, L0) {
+  sqrt((H0^2) / (2 * L0^2) + 1)
+}
+
+D_func <- function(H, R, L, L0) {
+  3 - log10(H / (sqrt(2) * L0 * sqrt(R^2 - 1))) / log10(L / L0)
+}
+
+HL0_func <- function(D, R, L, L0) {
+  (3 - D) * log10(L/L0) + 0.5 * log10(R^2 - 1)
+}
+
+# Main functions
+
+height_variation <- function(write=TRUE, return=FALSE) {
+  # Fractal dimension, D
+  temp <- data.frame()
+  for (s in scl) {
+    inc <- seq(0, L-s, s)
+    x <- rep(inc, L/s)
+    y <- rep(inc, each=L/s)
+    # If you can't get multicore (parallel) package working, change "mcmapply" to "mapply" below:
+    temp <- rbind(temp, data.frame(L0=s, x=x, y=y, H0=mcmapply(fd_func, x, y, s)))
+  }
+  # This variation method is time-consuming, and so save the result to avoid reprocessing if recalculting RDH
+  if (write) {
+    #write.csv(temp, paste0("output/var/var_", site, "_", rep, ".csv"), row.names = FALSE)
+    #write.csv(temp, paste0("/output/", output, "/var_", names(data), "_", sprintf("%04d", rep), ".csv"), row.names=FALSE)
+  }
+  #print(paste0("Complete: ", site[[1]][1], "_", sprintf("%04d", rep)))
+  # You can return the data and assign to variable if wish
+  if (return) {
+    return(temp)
+  }
+}
+
+rdh <- function(hvar) {
+  
+  # Mean of scales to avoid biased sampling at smaller scales
+  hvar_m <- aggregate(H0 ~ L0, hvar, mean)
+  # plot(log10(H0) ~ log10(L0), hvar_m)
+  
+  # Find the height ranges at both ends of the scale
+  H <- hvar_m$H0[hvar_m$L0 == max(hvar_m$L0)]
+  H0 <- hvar_m$H0[hvar_m$L0 == min(hvar_m$L0)]
+  
+  # Calculate slopes and minus from 3 (i.e., to get D from S) (FD from mean)
+  mod <- lm(log10(H0) ~ log10(L0), hvar_m)
+  D <- 3 - coef(mod)[2]
+  mod_ends <- lm(log10(H0) ~ log10(L0), hvar_m[c(1, nrow(hvar_m)),])
+  D_ends <- 3 - coef(mod_ends)[2]
+  
+  # Calculate rugosity from theory
+  R_theory <- R_func(H0, L0)		
+  
+  # Calculate rugosity from theory (integral)
+  HL0.all <- hvar$H0[hvar$L0==min(hvar$L0)]
+  R_theory2 <-  sum(R_func(HL0.all, L0)) / (2/L0)^2
+  
+  R <- NA
+  # Optional test: calculating R using another method
+  temp3 <- crop(data, extent(x0, x0 + L, y0, y0 + L))
+  temp3 <- as(temp3, 'SpatialGridDataFrame')
+  R <- surfaceArea(temp3) / L^2
+  
+  # Calculate D from theory
+  D_theory <- D_func(H, R_theory, L, L0)
+  
+  return(list(D=D, D_ends=D_ends, D_theory=D_theory, R=R, R_theory=R_theory, R_theory2=R_theory2, H=H)) 
+}
---
+++ R/noaa_surface_geometry.R
@@ -0,0 +1,122 @@
+
+
+##### GEOLOGICAL AND ENVIRONMENTAL FACTORS SHAPE REEF #####
+##### HABITAT STRUCTURE IN THE MAIN HAWAIIAN ISLANDS #####
+################### ASBURY ET AL. 2022 ###################
+
+library(rgeos)
+library(sp)
+source("R/functions.R")
+
+# DEMs & DEM extent 
+files <- list.files("data")
+
+# allocate space
+complexity <- data.frame()
+
+for (f in seq(1, 300, 2)){
+  
+  borderfilename = files[f]
+  infilename= files[f+1]
+  
+  # Load rasters (DEM & bounding extent)
+  temp.ras <- raster(paste0("data/", infilename))
+  data.b <- raster(paste0("data/", borderfilename))
+  plot(data)
+  
+  # Bounding box
+  a <- aggregate(data.b, fact = 100)
+  r <- a > -Inf
+  pp <- rasterToPolygons(r, dissolve = TRUE)
+  buff <- gBuffer(pp, width = -0.5)
+  ex <- extent(buff)
+  plot(buff, add = TRUE)
+  
+  # Scope (extent), scales of variation, and resolution (grain)
+  L <- 2 #2x2m box
+  scl <- L / c(1, 2, 3.125, 6.25, 12.5, 25, 50, 100, 200) #2m-1cm
+  L0 <- min(scl) 
+  resolution <- 0.01 
+  rep <- 1
+  i <- 0
+  j <- 0
+  site = strsplit(names(data), split = "_") 
+
+  # Delineate 5 boxes within a single plot
+  while (i < 5) {
+    
+    j <- j + 1
+    x0 <- runif(1, ex[1], ex[2])
+    y0 <- runif(1, ex[3], ex[4])
+    rect(x0, y0, x0 + L, y0 + L, border="red")
+    
+    data <- crop(temp.ras, extent(x0, x0 + L, y0, y0 + L))
+    
+    # Ensure box is fully inside plot
+    x.crd <- c(x0,x0,x0,x0+L/2,x0+L,x0+L,x0+L,x0+L/2)
+    y.crd <- c(y0,y0+L/2,y0+L,y0+L,y0+L,y0+L/2,y0,y0)
+    
+    overlap <- point.in.polygon(x.crd, y.crd, buff@polygons[[1]]@Polygons[[1]]@coords[,1], buff@polygons[[1]]@Polygons[[1]]@coords[,2])
+    
+    if(sum(overlap) == 8){
+      rect(x0, y0, x0 + L, y0 + L, border="blue")
+      
+      # Correct slopes
+      x <- seq(data@extent[1]+res(data)[1]/2, data@extent[2]-res(data)[1]/2, res(data)[1])
+      y <- seq(data@extent[3]+res(data)[2]/2, data@extent[4]-res(data)[2]/2, res(data)[2])
+      z <- data@data@values
+      xy <- expand.grid(y, x)
+      
+      mod <- lm(z ~ xy$Var1 + xy$Var2)
+      
+      data@data@values <- mod$residuals
+      
+      #plot(data)
+      #writeRaster(data, (paste0("output/rasters/", site, "_", rep, "_fixed.tif"))) 
+      
+      temp <- crop(data, extent(x0, x0 + L, y0, y0 + L))
+      i <- i + 1
+       
+      # Ensure no NAs in plot
+      na.fix <- table(is.na(temp[])) }}
+      if(is.na(na.fix[2])){
+      
+        # Extract habitat metrics
+        start.time <- Sys.time()
+        example <- height_variation(write=TRUE, return=TRUE)
+        out <- rdh(example)
+  
+        rep <- rep + 1
+        i <- i + 1
+        
+        complexity <- rbind(complexity, data.frame(site = site[[1]][1],
+                           box = i,
+                           x = x0,
+                           y = y0,
+                           R = out$R,
+                           R_theory = out$R_theory,
+                           D_sum = out$D,
+                           D_theory = out$D_theory,
+                           D_ends = out$D_ends,
+                           H = out$H))
+        
+        end.time <- Sys.time()
+        print(end.time - start.time)
+
+      }
+  
+  # Start on next plot if 5 boxes cannot fit 
+  if (j > 200000){
+    i = 5
+    print(paste0(site[[1]][1], "  too thin"))
+  }
+}
+  
+write.csv(complexity, "output/complexity.csv")
+ 
+
+
+
+
+
+
